45f6df855cc9a82480b7d837bdd8043132e02661
NestorArroyoSerrano <122863244+NestorArroyoSerrano@users.noreply.github.com>
2024-03-06 19:38:14 +0100

    update

--- src/main/java/com/nestor/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
+++ src/main/java/com/nestor/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
@@ -2,11 +2,14 @@ package com.nestor.curso.springboot.jpa.springbootjpa;
 
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.nestor.curso.springboot.jpa.springbootjpa.entities.Person;
 import com.nestor.curso.springboot.jpa.springbootjpa.repositories.PersonRepository;
@@ -20,13 +23,88 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 		SpringApplication.run(SpringbootJpaApplication.class, args);
 	}
 
+	@Transactional(readOnly = true)
+	public void findOne() {
+
+		/* 
+		Person person = null;
+		Optional<Person> optionalPerson = repository.findOneName("Pepe");
+		//String namePerson = optionalPerson.get().getName();
+		//if(!optionalPerson.isEmpty()) {
+		if(optionalPerson.isPresent()) {
+			person = optionalPerson.get();			
+		}
+		System.out.println(person);
+		*/
+		repository.findByNameContaining("hn").ifPresent(System.out::println);
+	}
+	@Transactional(readOnly = true) // Cuando son consultas
+	public void list() {
+	//List<Person> persons = (List<Person>) repository.findAll();
+	List<Person> persons = (List<Person>) repository.findByProgrammingLanguageAndName("Java", "Andres");
+
+	persons.stream().forEach(person->System.out.println(person));
+
+	List<Object[]> personsValues = repository.obtenerPersonDataByProgrammingLanguage("Java");
+	personsValues.stream().forEach(person->System.out.println(person[0] + " es experto en " + person[1]));
+//	personsValues.stream().forEach(person->System.out.println(person));
+
+
+	}
+
+	
+	@Transactional
+	public void create() {
+
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Ingrese el nombre:");
+		String name = sc.nextLine();
+		System.out.println("Ingrese el apellido:");
+		String lastname = sc.nextLine();
+		System.out.println("Ingrese el lenguaje de programación:");
+		String programmingLanguage = sc.nextLine();
+		sc.close();
+
+		Person person = new Person(null, name, lastname, programmingLanguage);
+		
+		Person personNew = repository.save(person);
+		System.out.println(personNew);
+
+		repository.findById(personNew.getId()).ifPresent(System.out::println);
+	}
+
+	@Transactional
+	public void update() {
+
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Ingrese el id de la persona: ");
+		Long id = sc.nextLong();
+		sc.nextLine();
+		Optional<Person> optionalPerson = repository.findById(id);
+
+		//optionalPerson.ifPresent(person-> {
+			if(optionalPerson.isPresent()){
+			Person person = optionalPerson.orElseThrow();
+			System.out.println(person);
+			System.out.println("Ingrese el lenguaje de programación:");
+			String programmingLanguage = sc.nextLine();
+			person.setProgrammingLanguage(programmingLanguage);
+			Person personDb = repository.save(person);
+			System.out.println(personDb);
+			} else {
+				System.out.println("El usuario no existe ");
+			}
+			
+	//	});
+		sc.close();
+	}
+
 	@Override
 	public void run(String... args) throws Exception {
-		
-		//List<Person> persons = (List<Person>) repository.findAll();
-		List<Person> persons = (List<Person>) repository.findByProgrammingLanguageAndName("Java", "Andres");
 
-		persons.stream().forEach(person->System.out.println(person));
+		//findOne();
+		//create();
+		update();
 	}
 
 }
